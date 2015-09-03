36bf134313e482e57cb32ce0e916da3ced4e3030
Vyacheslav Akhalaya <akhalaya@Private-051.zfort.net>
2015-09-03 12:41:15 +0300

    up plugin version

--- index.php
+++ index.php
@@ -4,5 +4,12 @@
   Plugin Name: test plugin udpate
   Plugin URI: http://www.bluefountainmedia.com/
   Description: Plugin test
-  Version: 0.0.1
+  Version: 0.0.2
  */
+require 'plugin-update-checker/plugin-update-checker.php';
+$className = PucFactory::getLatestClassVersion('PucGitHubChecker');
+$myUpdateChecker = new $className(
+    'https://github.com/SlavaBFM/testrepo',
+    __FILE__,
+    'master'
+);
